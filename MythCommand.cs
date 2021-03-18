8e60cef7f3d27b086115e0b4f4a6ab0acced86bf
unknown <fancygiving7@gmail.com>
2021-03-18 22:04:04 +0300

    Initial Release

---
+++ MythCommand.cs
@@ -0,0 +1,82 @@
+﻿using SDG.Provider;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+using Rocket.Unturned.Player;
+using System.Collections.Generic;
+
+namespace Mythicals
+{
+    public class MythCommand : IRocketCommand
+    {
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+        public string Name => "myth";
+        public string Help => "This command will allow you to equip any community market hat!";
+        public string Syntax => "/myth <name/id>";
+        public List<string> Aliases => new List<string>() { "mythical" };
+        public List<string> Permissions => new List<string>() { "myth" };
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            if (command.Length < 1)
+            {
+                UnturnedChat.Say(caller, Syntax);
+                return;
+            }
+
+            UnturnedPlayer client = caller as UnturnedPlayer;
+            List<UnturnedEconInfo> list = TempSteamworksEconomy.econInfo;
+
+            if (int.TryParse(command[0], out int hatId))
+            {
+                if(hatId == 0)
+                {
+                    Mythicals.Instance.RegisterPlayer(client.CSteamID.m_SteamID, hatId);
+                    UnturnedChat.Say(caller, "Success! Reconnect to apply changes.");
+                    return;
+                }
+
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if(list[i].itemdefid == hatId)
+                    {
+                        if(list[i].type.ToLower().Contains("hat"))
+                        {
+                            Mythicals.Instance.RegisterPlayer(client.CSteamID.m_SteamID, hatId);
+                            UnturnedChat.Say(caller, "Successfully applied " + list[i].name + ". Reconnect to apply changes.");
+                            return;
+                        }
+                        else
+                        {
+                            UnturnedChat.Say(caller, list[i].name + " is not a hat.");
+                            return;
+                        }
+                    }
+                }
+                
+                UnturnedChat.Say(caller, "The hat id you specified does not exist.");
+            }
+            else 
+            {
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i].name.ToLower().Contains(command[0].ToLower()))
+                    {
+                        if (list[i].type.ToLower().Contains("hat"))
+                        {
+                            Mythicals.Instance.RegisterPlayer(client.CSteamID.m_SteamID, hatId);
+                            UnturnedChat.Say(caller, "Successfully applied " + list[i].name + ". Reconnect to apply changes.");
+                            return;
+                        }
+                        else
+                        {
+                            UnturnedChat.Say(caller, list[i].name + " is not a hat.");
+                            return;
+                        }
+                    }
+                }
+
+                UnturnedChat.Say(caller, "The hat name you specified does not exist.");
+            }
+        }
+    }
+}
---
+++ Mythicals.cs
@@ -0,0 +1,95 @@
+﻿using Rocket.Core.Plugins;
+using Rocket.Unturned.Permissions;
+using SDG.Unturned;
+using Steamworks;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace Mythicals
+{
+    class Mythicals : RocketPlugin
+    {
+        public const string JSON_FILE = "Plugins/Mythicals/players_myths.json";
+        public static Mythicals Instance { get; private set; }
+        public List<JsonInfo> players { get; set; }
+
+        protected override void Load()
+        {
+            Instance = this;
+            LoadCFG();
+            UnturnedPermissions.OnJoinRequested += new UnturnedPermissions.JoinRequested(OnPlayerConnect);
+        }
+
+        public void LoadCFG()
+        {
+            try
+            {
+                if (File.Exists(JSON_FILE))
+                {
+                    string json = File.ReadAllText(JSON_FILE);
+                    players = JsonConvert.DeserializeObject<List<JsonInfo>>(json);
+                }
+                else
+                {
+                    players = new List<JsonInfo>();
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Failed to load 'players_myths.json'.");
+                Logger.LogException(ex);
+            }
+        }
+
+        public void RegisterPlayer(ulong steamid, int hatId)
+        {
+            for (int i = 0; i < players.Count; i++)
+            {
+                if(players[i].steamid == steamid)
+                {
+                    if (hatId != 0)
+                        players[i].hatId = hatId;
+                    else
+                        players.RemoveAt(i);
+
+                    File.WriteAllText(JSON_FILE, JsonConvert.SerializeObject(players));
+                    return;
+                }
+            }
+
+            players.Add(new JsonInfo { steamid = steamid, hatId = hatId });
+            File.WriteAllText(JSON_FILE, JsonConvert.SerializeObject(players));
+        }
+
+        protected override void Unload()
+        {
+            Instance = null;
+            UnturnedPermissions.OnJoinRequested -= new UnturnedPermissions.JoinRequested(OnPlayerConnect);
+        }
+
+        public void OnPlayerConnect(CSteamID Player, ref ESteamRejection? rejection)
+        {
+            foreach (SteamPending Players in Provider.pending)
+            {
+                if (Players.playerID.steamID == Player)
+                {
+                    JsonInfo info = players.FirstOrDefault(x => x.steamid == Player.m_SteamID);
+                    if (info != null)
+                    {
+                        Players.hatItem = info.hatId;
+                    }
+                }
+            }
+        }
+    }
+
+    class JsonInfo
+    {
+        public ulong steamid;
+        public int hatId;
+    }
+}
